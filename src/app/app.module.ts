db2e777bd8b2403a166a29173c9d1629b03badbe
Javier Moreno Valle <javimoreno.tec@gmail.com>
2018-10-03 18:47:47 +0200

    Book List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,34 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { BookListPage } from '../pages/book-list/book-list';
+import { BookService } from '../services/book-service';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    BookListPage
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    BookListPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    BookService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
